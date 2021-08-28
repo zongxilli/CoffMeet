b627523074dece879825b835cd761999958605b9
zongxilli <zongxi2014@gmail.com>
2021-08-28 15:54:42 -0400

    added sampleData

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import EventForm from '../eventForm/EventForm';
+import EventList from './EventList';
+import { sampleData } from '../../../app/api/sampleData';
 
 export default function EventDashboard() {
 	return (
 		<Grid>
 			<Grid.Column width={10}>
-				<h2>Left Column</h2>
+				<EventList events={sampleData} />
 			</Grid.Column>
 			<Grid.Column width={6}>
-				<h2>Right Column</h2>
+				<EventForm />
 			</Grid.Column>
 		</Grid>
 	);
